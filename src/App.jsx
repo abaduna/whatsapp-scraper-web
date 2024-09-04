2c28456fd6635ef068e3b6c27fef16658b9be277
abaduna <artuelrey@gmail.com>
2024-09-04 13:50:03 -0300

    feat:gyms

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,60 @@ import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [id, setId] = useState(1);
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [id, setId] = useState(156);
+  const [phoneNumber, setPhoneNumber] = useState("341592493");
   const [buttonColor, setButtonColor] = useState(''); // Estado para el color del botón
-
+  const [res, setres] = useState({})
   const sendWh = () => {
     // Mensaje predefinido
-    const message = "¡Hola! Este es un mensaje predefinido.";
+   if (typeof phoneNumber != "string" || phoneNumber == null) {
+    return
+   }
+    
+    const message = `¡Transforma la gestión de tu gimnasio con nuestra solución integral!
+
+Estimado,Soy Arturo
+
+¿Estás cansado de tener que recordar quién te pagó el mes y quién no? Te presento nuestro sistema de gestión de Gimnasios:
+
+Todos tus usuarios activos con una foto para poder recordarlo
+Un botón para mandarles un WhatsApp para poder comunicarte
+Se pone en rojo cuando no pagaron el mes, fácil de identificar
+Descripción para que no te olvides de algo en particular
+
+¿Te gustaría saber más o agendar una demostración? Estoy a tu disposición para ayudarte a llevar tu gimnasio al siguiente nivel.
+
+¡Espero poder colaborar contigo pronto!
+
+Saludos cordiales`;
+    
     const phoneNumberWithoutSpaces = phoneNumber.replace(/\s+/g, '').replace(/^0/, '');
+    
     if (phoneNumberWithoutSpaces[3] === "4") {
-      console.log('no me puede mandar wh');
+      const whatsappUrl = `https://wa.me/549${phoneNumberWithoutSpaces}?text=${encodeURIComponent(message)}`;
+      window.open(whatsappUrl, "_blank");
       // Aquí podrías bloquear el envío o manejar la lógica como prefieras
     } else {
       const whatsappUrl = `https://wa.me/549${phoneNumberWithoutSpaces}?text=${encodeURIComponent(message)}`;
       window.open(whatsappUrl, "_blank");
     }
-  };
+};
 
   const newPhone = async () => {
     const apiUrl = `http://localhost:8080/api/phone/${id}`;
     try {
       const phone = await fetch(apiUrl);
       const data = await phone.json();
-      setPhoneNumber(data.number);
+      setres(data)
+
+      console.log('res', res)
+      if (data.number) {
+       setPhoneNumber(data.number) 
+      }
+      
+    
+
+      console.log('data', data)
       setId((prev) => prev + 1);
     } catch (error) {
       console.log(error);
@@ -46,7 +77,7 @@ function App() {
       {id}
       <button onClick={sendWh} style={{ backgroundColor: buttonColor }}>MAndar wh</button>
       <button onClick={newPhone}>Nuevo numero</button>
-      <a href="https://api.whatsapp.com/send?phone=5493413592493">Haz clic aquí</a>
+      <h1>{res?.number === null ? "Nulo" : res?.number}</h1>
     </>
   )
 }
